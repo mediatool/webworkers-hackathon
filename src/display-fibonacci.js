87d4ffc875043f5279625e590618dafa9eed3a94
Magnus Ohlin <magnus@mediatool.com>
2019-08-28 11:29:55 +0200

    Exploring hooks and css in js

--- src/display-fibonacci.js
+++ src/display-fibonacci.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react'
 import parseNumberAnd from './parse-number-and'
+import Loader from './loader'
+import './fibonacci.css'
 
-const fibWorker = new Worker(process.env.PUBLIC_URL + '/nth-fibonacci.js')
+const fibWorker = new Worker(process.env.PUBLIC_URL + '/nth-fibonacci.js') // eslint-disable-line
 
 function DisplayFibonacci () {
   const [ wantedNumber, setNumber ] = useState(1)
   const [ calculatedValue, setCalculatedValue ] = useState(1)
+  const [ isLoading, setIsLoading ] = useState(false)
 
   fibWorker.onmessage = function (e) {
     setCalculatedValue(e.data)
+    setIsLoading(false)
   }
 
   function handleChange (n) {
+    setIsLoading(true)
     setNumber(n)
     fibWorker.postMessage(n)
   }
@@ -20,17 +25,20 @@ function DisplayFibonacci () {
     <div>
       <div className="bp3-callout .modifier">
         <h4 className="bp3-heading">Fibonacci</h4>
-        Type in a number n to find out what the nth fibonacci number is.
+        Type in a number n to find out what the nth fibonacci number is.<br />
         Somewhere just below 40 it starts to take time to compute.
+        <br />
         <br />
         <input
           type="number"
           min="1"
           value={ wantedNumber }
           onChange={ parseNumberAnd(handleChange) }
+          disabled={ isLoading }
         />
         <br />
-        Fibonacci number { wantedNumber } is { calculatedValue }
+        <h4 className="fib-number">Fibonacci number { wantedNumber } is </h4>
+        <div className="fib-answer">{ isLoading ? <Loader /> : <code>{ calculatedValue }</code> }</div>
       </div>
     </div>
   )
